8e95dee043acc7d5bdfd74244d336c2d8be1f279
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-04-03 09:35:21 +0800

    Added gunicorn and removed debug mode

--- run.py
+++ run.py
@@ -1,4 +1,26 @@
 from app import app
+from multiprocessing import cpu_count
+from gunicorn.app.base import BaseApplication
+
+class FlaskApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        for key, value in self.options.items():
+            self.cfg.set(key, value)
+
+    def load(self):
+        return self.application
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    options = {
+        'bind': '0.0.0.0:5000',
+        'workers': cpu_count() * 2 + 1
+    }
+
+    FlaskApplication(app, options).run()
+    
